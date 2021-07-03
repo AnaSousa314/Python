8322a30f1c7a87081dfac566c635c1e33d1d720c
Ana.Sousa <anasousa314@outlook.com>
2021-07-03 17:44:56 -0300

    01-Basics/03-Tipos de Variaveis em python

---
+++ 01-Basics/03-Tipos_de_Variaveis.py
@@ -0,0 +1,14 @@
+x=-10
+y=19.99
+escola="Let's Code"
+cidade='São Paulo'#com aspas simples nao podemos colocar uma aspa simples dentro dela como na variavel escola,só podemos fazer isso com aspas duplas
+
+disponivel=True #valores booleanos sempre sao escritos com letra maiuscula
+indisponivel=False 
+
+print(y,type(y))
+print(x,type(x))
+print(escola,type(escola))
+print(cidade,type(cidade))
+print(disponivel,type(disponivel))
+print(indisponivel,type(indisponivel))
